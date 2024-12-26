7393005cb94860227157e10917268f97378a5974
robotGentil <laure.waziers@gmail.com>
2024-12-26 19:14:36 +0100

    nous avons rajouté une question 4

--- main.py
+++ main.py
@@ -19,6 +19,39 @@ os.system("cls" or "clear")
 
 #defining global variables
 SLEEP_BETWEEN_TEXT = 0
+QUESTIONS = [{
+        "question": "Que représente le symbole T, à l'envers?",
+        "reponses": [
+            "A - Médiatrice",
+            "B - Perpendiculaire"
+        ],
+        "reponse_correcte": "B"
+    },
+    {
+        'question': "Que représente le symbole  //?",
+        'reponses': [
+            "A - parallèle",
+            "B - opposés"
+        ],
+        'reponse_correcte': "A"
+    },
+    {
+        'question': "Que représente le symbole x?",
+        'reponses': [
+            "A -fois ",
+            "B -plus"
+        ],
+        'reponse_correcte': "A"
+    },
+    {
+        'question': "Quel est le calcul à faire en 1er dans 16 + 453 x 352?",
+        'reponses': [
+            "A - 453 x 352",
+            "B - 16 + 453"
+        ],
+        'reponse_correcte': "A"
+    }
+]
 
 
 def print_pythagore(text, time_sleep=SLEEP_BETWEEN_TEXT):
@@ -29,46 +62,9 @@ def print_pythagore(text, time_sleep=SLEEP_BETWEEN_TEXT):
 def input_pythagore(text):
     return input(Fore.GREEN+ text + "\n" + Style.RESET_ALL)
 
-def poser_question_une():
-    print_pythagore("Que représente le symbole T, à l'envers?")
-    print_pythagore("A - Médiatrice")
-    print_pythagore("B - Perpendiculaire")
-    reponse = input_pythagore("Quelle est ta réponse ?")
-    if (reponse == "B"):
-        print_pythagore("Bonne réponse")
-        return "Correct"
-    else:
-        print_pythagore("Mauvaise réponse")
-        return "Faux"
-    
 
-def poser_question_deux():
-    print_pythagore("Que représente le symbole  //?")
-    print_pythagore("A - parallèle")
-    print_pythagore("B - opposés")
-    reponse = input_pythagore("Quelle est ta réponse ?")
-    if (reponse == "A"):
-        print_pythagore("Bonne réponse")
-        return "Correct"
-    else:
-        print_pythagore("Mauvaise réponse")
-        return "Faux"
-    
-def poser_question_trois():
-    print_pythagore("Que représente le symbole ?")
-    print_pythagore("A - ")
-    print_pythagore("B -")
-    reponse = input_pythagore("Quelle est ta réponse ?")
-    if (reponse == "A"):
-        print_pythagore("Bonne réponse")
-        return "Correct"
-    else:
-        print_pythagore("Mauvaise réponse")
-        return "Faux"
-    
+
 if __name__ == '__main__':
-
-
     print_pythagore(        "Bonjour, je m'appelle Pythagore, je vais vous interroger sur vos compétences en Mathématiques")
     name = input_pythagore("Comment t'appelles tu ?")
     print_pythagore("Bonjour " + name + ", nous allons tout de suite commencer !")
@@ -76,23 +72,22 @@ if __name__ == '__main__':
     nombre_de_reponses_justes = 0
     nombre_de_questions_posées = 0
     
-    resultat = poser_question_une()
-    nombre_de_questions_posées = nombre_de_questions_posées + 1
-    if (resultat == "Correct"):
-        nombre_de_reponses_justes = nombre_de_reponses_justes + 1
+    for question in QUESTIONS:
+        print_pythagore(question["question"])
+        for reponse in question["reponses"]:
+            print_pythagore(reponse)
+        reponse = input_pythagore("Quelle est ta réponse ?")
+        if (reponse == question["reponse_correcte"]):
+            print_pythagore("Bonne réponse")
+            nombre_de_reponses_justes = nombre_de_reponses_justes + 1
+        else:
+            print_pythagore("Mauvaise réponse")
+        nombre_de_questions_posées = nombre_de_questions_posées + 1
 
 
-    resultat = poser_question_deux()
-    nombre_de_questions_posées = nombre_de_questions_posées + 1
-    if (resultat == "Correct"):
-        nombre_de_reponses_justes = nombre_de_reponses_justes + 1
-
-    resultat = poser_question_trois()
-    nombre_de_questions_posées = nombre_de_questions_posées + 1
-    if (resultat == "Correct"):
-        nombre_de_reponses_justes = nombre_de_reponses_justes + 1
 
 
+       
     print_pythagore("Bravo tu as répondu correctement à " + str(nombre_de_reponses_justes) + " questions sur " + str(nombre_de_questions_posées))
 
 
